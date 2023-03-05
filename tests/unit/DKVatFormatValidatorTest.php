0b372ae5b31a7b48edcffc37eb125280901f1346
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-03-05 18:43:43 +0400

    [CVFV-2] setup test.

--- tests/unit/DKVatFormatValidatorTest.php
+++ tests/unit/DKVatFormatValidatorTest.php
@@ -1,22 +1,21 @@
 <?php
 
-// TODO: set namespace
-namespace unit;
+namespace rocketfellows\DKVatFormatValidator\tests\unit;
 
-class CountryVatFormatValidatorTest extends TestCase
+use PHPUnit\Framework\TestCase;
+
+class DKVatFormatValidatorTest extends TestCase
 {
     /**
-     * TODO: set type
-     * @var
+     * @var DKVatFormatValidator
      */
     private $validator;
 
-    /**
-     * TODO: setup validator for test
-     */
     protected function setUp(): void
     {
-        $this->validator = new CountryVatFormatValidator();
+        parent::setUp();
+
+        $this->validator = new DKVatFormatValidator();
     }
 
     /**
