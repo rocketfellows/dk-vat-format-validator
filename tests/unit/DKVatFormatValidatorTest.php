d0ba881d6e3fa73ccf66c1e50aeb2748075d3ac5
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-03-05 18:51:30 +0400

    [CVFV-2] added test cases.

--- tests/unit/DKVatFormatValidatorTest.php
+++ tests/unit/DKVatFormatValidatorTest.php
@@ -30,8 +30,80 @@ class DKVatFormatValidatorTest extends TestCase
     {
         return [
             [
-                'vatNumber',
-                'isValid',
+                'vatNumber' => 'DK12345678',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => 'DK00000000',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => 'DK11111111',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => 'DK99999999',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => '12345678',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => '00000000',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => '11111111',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => '99999999',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => 'DK123456789',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => 'DK1234567',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => '123456789',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => '1234567',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => 'Dk12345678',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => 'dK12345678',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => 'dk12345678',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => 'DE12345678',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => 'DK12345678A',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => '1',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => '',
+                'isValid' => true,
             ],
         ];
     }
